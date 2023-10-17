c78eabe06fa20efdeb579da822b2330e0d681a5b
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-10-17 01:19:20 -0400

    Move styles object to its own file

--- public/scripts/your-css.js
+++ public/scripts/your-css.js
@@ -1,8 +1,5 @@
+import { styles } from '../../src/objects/styles';
 var link = document.createElement('style');
-;
-export var styles = {
-    stackoverflow: 'body {background-color: black} header {background-color: yellow}',
-};
 var url = new URL(window.location.href);
 var domain = url.hostname.split('.')[0];
 link.appendChild(document.createTextNode(styles[domain]));
--- public/scripts/your-css.ts
+++ public/scripts/your-css.ts
@@ -1,12 +1,7 @@
+import { styles } from '../../src/objects/styles'
+
 const link: HTMLStyleElement = document.createElement('style');
 
-interface Style {
-  [key: string] : string
-};
-
-export const styles: Style = {
-  stackoverflow: 'body {background-color: black} header {background-color: yellow}',
-};
 const url = new URL(window.location.href);
 const domain: string | undefined = url.hostname.split('.')[0];
 
