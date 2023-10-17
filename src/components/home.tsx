4517991c1c92a87d45d1cd3f1fb71d5bbe6d1b70
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-10-17 01:29:50 -0400

    Let more than one listing be rendered add top bar

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,24 +1,28 @@
-import Listing from '../components/listing.js'
-import { styles } from '../../public/scripts/your-css.js'
+import Listing from './listing'
+import { styles } from '../objects/styles'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
   const passStyles = () => {
+    const stylesArray = []
     for (let style in styles) {
       if (styles.hasOwnProperty(style)) {
-        return (
-          <>
-            <Listing style={ style } styles={ styles }/>
-          </>
+        stylesArray.push(
+          <Listing key={Math.random()} style={ style } styles={ styles }/>
         )
       }
     }
+    return stylesArray
   }
 
   return (
     <>
+      <label htmlFor="search"></label>
+      <input type="text" id='search' />
+      <Link to='/form'><button>+</button></Link>
       <div>
-        {passStyles()}
+        {passStyles().map(listing => listing)}
       </div>
     </>
   )
