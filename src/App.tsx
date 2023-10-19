309a413076a635fd556c3dc9313aaea293fb3963
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-10-18 23:08:07 -0400

    Chnage to memory router, change edit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
+import { Routes, Route, MemoryRouter } from "react-router-dom";
 import './App.css';
-import { styles } from '../public/scripts/your-css.js';
-import Listing from './components/listing.js';
 import NavBar from './components/nav.js'
+import Form from "./components/form.js";
 import Home from "./components/home.js";
 
 function App() {
 
-  const passStyles = () => {
-    for (let style in styles) {
-      if (styles.hasOwnProperty(style)) {
-        return (
-          <>
-            <Listing style={ style } styles={ styles }/>
-          </>
-        )
-      }
-    }
-  }
-
   return (
     <div>
-      <BrowserRouter>
+      <MemoryRouter>
         <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path="/form"  element={<Form /> } />
-          <Route path="/form/:site"  element={<Form /> } />
+          <Route path="/form"  element={<Form website="" customCss="" /> } />
         </Routes>
-      </BrowserRouter>
-      {passStyles()}
+      </MemoryRouter>
     </div>
   )
 }
