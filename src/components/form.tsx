0c78e75da74504d38d2fd046f7a25d430be2e275
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-10-18 23:05:25 -0400

    Make form show text from props for edits.

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,10 +1,20 @@
-const Form = () => {
+import { useState } from "react"
+
+interface FormProps {
+  website: string
+  customCss: string
+};
+
+const Form = (props: FormProps) => {
+  const [websiteInput, setWebsiteInput] = useState(props.website || '');
+  const [cssInput, setCssInput] = useState(props.customCss || '')
+
   return (
     <>
       <label htmlFor="website-input">Website</label>
-      <input type="text" id="website-input"></input>
+      <input type="text" id="website-input" name="website" onChange={(e) => setWebsiteInput(e.target.value)} value={websiteInput}/>
       <label htmlFor="css-input">custom css</label>
-      <textarea name="css-input" id="css-input" ></textarea>
+      <textarea name="css-input" id="css-input" onChange={(e) => setCssInput(e.target.value)} value={cssInput}/>
     </>
   )
 }
