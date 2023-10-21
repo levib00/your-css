44fb27044a71a5b186e2900255084432d4b668fa
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-10-20 21:42:46 -0400

    Adds submissions to object.

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { styles } from "../objects/styles"
 
 interface FormProps {
   website: string
@@ -9,12 +10,17 @@ const Form = (props: FormProps) => {
   const [websiteInput, setWebsiteInput] = useState(props.website || '');
   const [cssInput, setCssInput] = useState(props.customCss || '')
 
+  const saveCss = (website: string, css: string) => {
+    styles[website] = css
+  }
+
   return (
     <>
       <label htmlFor="website-input">Website</label>
-      <input type="text" id="website-input" name="website" onChange={(e) => setWebsiteInput(e.target.value)} value={websiteInput}/>
+      <input type="text" id="website-input" name="website" onChange={(e) => {setWebsiteInput(e.target.value); console.log('changed')}} value={websiteInput}/>
       <label htmlFor="css-input">custom css</label>
       <textarea name="css-input" id="css-input" onChange={(e) => setCssInput(e.target.value)} value={cssInput}/>
+      <button onClick={() => saveCss(websiteInput, cssInput)}>save</button>
     </>
   )
 }
