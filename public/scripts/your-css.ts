abb2259a907cf4830c67ed6904291e837a52cb44
Levi B <levib2000@gmail.com>
2024-02-15 05:24:18 -0500

    Set styles from browser storage

--- public/scripts/your-css.ts
+++ public/scripts/your-css.ts
@@ -1,9 +1,12 @@
-import { styles } from '../../src/objects/styles'
+import { styles, setStyles } from '../../src/objects/styles'
+import { getFromStorage } from '../../src/scripts/storage-handlers'
 
 const link: HTMLStyleElement = document.createElement('style');
 
 const url = new URL(window.location.href);
 const domain: string | undefined = url.hostname.split('.')[0];
+
+setStyles(getFromStorage());
 
 link.appendChild(document.createTextNode(styles[domain]));
 document.head.appendChild(link);
