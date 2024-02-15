8e02eec458879e46786160d5a1db1b607aa7d2cc
Levi B <levib2000@gmail.com>
2024-02-15 05:17:11 -0500

    Add edit and delete buttons and functions

--- src/components/listing.tsx
+++ src/components/listing.tsx
@@ -1,18 +1,47 @@
+import Form from "./form";
+import { styles as masterStyles } from "../objects/styles";
+import { useState } from "react";
+import { saveToStorage } from "../scripts/storage-handlers";
+
 interface ListingProps {
   style : string
-  styles :{ [key: string]: string}
+  styles :{ [key: string]: string} // TODO: fix these names.
+  setAllStyles: any
 };
 
 function Listing(props: ListingProps) {
+  const {style, setAllStyles} = props
+  const [editMode, setEditMode] = useState(false)
+
+  const deleteListing = () => {
+    delete masterStyles[style];
+    saveToStorage(masterStyles)
+    setAllStyles({...masterStyles})
+    // TODO: use DI instead of props.
+  }
+
+  const openEditPage = () => {
+    setEditMode(true)
+    // TODO: cancel button on form is gonna have to set this to false when pressed and isEdit
+   }
 
   return (
     <>
-      <div>
-        { props.style }
-      </div>
-      <div>
-        { props.styles[props.style] }
-      </div>
+      {
+        editMode ?
+        <Form website={style} customCss={props.styles[style]}/>
+        : 
+        <>
+          <div>
+            { style }
+          </div>
+          <div>
+            { props.styles[style] }
+          </div>
+          <button onClick={openEditPage}>edit</button>
+          <button onClick={deleteListing}>delete</button>
+        </>
+      }
     </>
   )
 }
