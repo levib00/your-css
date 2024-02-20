56a115d015378841ffa983cf24e93cc74a642937
Levi B <levib2000@gmail.com>
2024-02-19 23:10:38 -0500

    move to dist

--- public/scripts/your-css.js
+++
@@ -1,6 +0,0 @@
-import { styles } from '../../src/objects/styles';
-var link = document.createElement('style');
-var url = new URL(window.location.href);
-var domain = url.hostname.split('.')[0];
-link.appendChild(document.createTextNode(styles[domain]));
-document.head.appendChild(link);
