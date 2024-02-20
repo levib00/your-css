2840ce2ad7264faccbccc8f8a09cac9c096af52b
Levi B <levib2000@gmail.com>
2024-02-19 23:09:57 -0500

    Add setup file

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,14 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-      "^.+\\.tsx?$": "ts-jest" 
+      "^.+\\.tsx?$": "ts-jest",
+      '^.+\\.(js|jsx)$': 'babel-jest'
   // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
       '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
   },
+  setupFiles: [
+    "./src/tests/setup/filemock"
+  ]
 }
