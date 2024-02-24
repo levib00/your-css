0f0cc514c3c469dfa40cc5148d02c57643d8cae6
Levi B <levib2000@gmail.com>
2024-02-23 22:30:50 -0500

    Add test to toggle turning app off

--- src/tests/scripts.test.tsx
+++ src/tests/scripts.test.tsx
@@ -1,5 +1,6 @@
 
 import { getStyleValue } from '../../public/scripts/your-css'
+import { setStyles } from '../objects/styles';
 
 describe("your-css style", () => {
 
@@ -11,16 +12,45 @@ describe("your-css style", () => {
     domain2: {
       isActive: false,
       css: 'css'
+    },
+    _toggleAll: {
+      isActive: false,
+      css: ''
     }
   }
 
+  beforeEach(() => {
+    setStyles(stylesMock)
+  })
+
   test('Active style returns css', () => {
+    const stylesMockIsActive = {
+      domain1: {
+        isActive: true,
+        css: 'css'
+      },
+      domain2: {
+        isActive: false,
+        css: 'css'
+      },
+      _toggleAll: {
+        isActive: true,
+        css: ''
+      }
+    }
+
+    setStyles(stylesMockIsActive)
     const mock111 = getStyleValue(stylesMock.domain1)
     expect(mock111).toBe('css')
   });
+
   test('Inactive style returns null', () => {
     const mock111 = getStyleValue(stylesMock.domain2)
     expect(mock111).toBeNull()
   });
 
+  test('Inactive style returns null', () => {
+    const mock111 = getStyleValue(stylesMock.domain1)
+    expect(mock111).toBeNull()
+  });
 })
