8f45a03ed2c0ed5f8d0880519a4a2e4d6154ed9c
Levi B <levib2000@gmail.com>
2024-02-26 05:38:24 -0500

    Allow styling of extension

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { styles } from './objects/styles'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -8,3 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+const domainStyle: HTMLStyleElement = document.createElement('style');
+domainStyle.appendChild(document.createTextNode(styles._extension?.isActive ? styles._extension?.css : ''));
+document.head.appendChild(domainStyle);
