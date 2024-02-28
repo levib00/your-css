cb21b0a87a4094a1eda4e4cb522f8cca6a8efa28
Levi B <levib2000@gmail.com>
2024-02-28 04:34:24 -0500

    Add search function

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,5 @@
 import Listing from './listing';
-import { styles } from '../objects/styles';
+import { styles, IStyle } from '../objects/styles';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
@@ -7,23 +7,50 @@ const Home = () => {
 
   const [allStyles, setAllStyles] = useState(styles)
   const [listings, setListings] = useState<any>([])
+  const [searchQuery, setSearchQuery] = useState('')
+  const [searchResults, setSearchResults] = useState({})
+
+  const func = (obj:  IStyle, searchQuery: string) => {
+    const op = Object.getOwnPropertyNames(obj)
+    const arr: IStyle  = {}
+    for ( let i: number = 0; i < op.length; i++) {
+      if (op[i].toLowerCase().includes(searchQuery)) {
+        arr[op[i]] = obj[op[i]]
+      }
+    }
+    setSearchResults(arr)
+    return arr
+  }
+
+  useEffect(() => {
+    func(allStyles, searchQuery)
+  }, [searchQuery])
 
   useEffect(() => {
       const stylesArray = []
+      if (searchQuery) {
+        for (let style in searchResults) {
+          if (styles.hasOwnProperty(style)) {
+            stylesArray.push(
+              <Listing key={Math.random()} style={ style } styles={ styles[style] } setAllStyles={setAllStyles} />
+            )
+          }
+        }
+      } else {
       for (let style in allStyles) {
         if (styles.hasOwnProperty(style)) {
           stylesArray.push(
             <Listing key={Math.random()} style={ style } styles={ styles[style] } setAllStyles={setAllStyles} />
-          )
+          )}
         }
       }
       setListings(stylesArray)
-  }, [allStyles])
+  }, [allStyles, searchResults])
 
   return (
     <>
       <label htmlFor="search"></label>
-      <input type="text" id='search' />
+      <input type="text" id='search' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
       <Link to='/form'><button>+</button></Link>
       <div>
         {listings.map((listing: any) => listing)}
