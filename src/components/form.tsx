bfc40fdc2a4487719c1703ee2fe1cbda6f09e85e
Levi B <levib2000@gmail.com>
2024-03-01 04:27:01 -0500

    Add isActive checkbox

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -6,20 +6,22 @@ import { useNavigate } from "react-router-dom";
 interface FormProps {
   website: string
   customCss?: string
+  isActive?: boolean
 };
 
 const Form = (props: FormProps) => {
   const [websiteInput, setWebsiteInput] = useState(props.website || '');
   const [cssInput, setCssInput] = useState(props.customCss || '')
+  const [isActive, setIsActive] = useState(props.isActive || false)
   const navigate = useNavigate();
 
   // TODO: add a warning if it isn't an edit. allow user to confirm update. 
-  const saveCss = (website: string, css: string) => {
+  const saveCss = (website: string, css: string, isActive: boolean) => {
     if (props.website !== website) {
       styles[website] = styles[props.website];
       delete styles[props.website];
     }
-    styles[website] = {isActive: true, css} // TODO: allow changing isActive
+    styles[website] = {isActive, css} // TODO: allow changing isActive
     saveToStorage(styles)
     navigate('/')
   }
@@ -30,7 +32,9 @@ const Form = (props: FormProps) => {
       <input type="text" id="website-input" name="website" onChange={(e) => setWebsiteInput(e.target.value)} value={websiteInput}/>
       <label htmlFor="css-input">custom css</label>
       <textarea name="css-input" id="css-input" onChange={(e) => setCssInput(e.target.value)} value={cssInput}/>
-      <button onClick={() => saveCss(websiteInput, cssInput)}>save</button>
+      <label htmlFor="active-checkbox">activate</label>
+      <input type="checkbox" id="active-checkbox" checked={isActive} onChange={() => {setIsActive(!isActive)}} />
+      <button onClick={() => saveCss(websiteInput, cssInput, isActive)}>save</button>
     </>
   )
 }
