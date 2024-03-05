4bd0c59bc6eddafc3257088a07a55ab43f329fda
Levi B <levib2000@gmail.com>
2024-03-04 22:55:58 -0500

    Export function fires on button press

--- src/tests/settings.test.tsx
+++ src/tests/settings.test.tsx
@@ -3,6 +3,9 @@ import { render, screen } from '@testing-library/react';
 import Settings from '../components/settings';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import * as importExport from '../scripts/import-export-css';
 
 describe('Settings page component',() => {
   test('Renders properly', () => {
@@ -16,5 +19,23 @@ describe('Settings page component',() => {
     expect(importButton).toBeInTheDocument();
     const exportButton = screen.getByText('export')
     expect(exportButton).toBeInTheDocument();
+  })
+
+  test('export fires', async () => {
+    const mock = jest.spyOn(importExport, 'assembleCssForExport').mockImplementation(jest.fn())
+    render(
+      <MemoryRouter>
+        <Settings />
+      </MemoryRouter>
+    );
+
+    const exportButton = screen.getByText('export');
+    expect(exportButton).toBeInTheDocument();
+
+    await act(async () => {
+      await userEvent.click(exportButton)
+    })
+
+    expect(mock).toHaveBeenCalledTimes(1)
   })
 })
