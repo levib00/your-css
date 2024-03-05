003cd4804822a2a45ec2ef5d1388d2715b92048c
Levi B <levib2000@gmail.com>
2024-03-04 22:56:46 -0500

    Add export function to button

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,9 +1,12 @@
+import { styles as masterStyles } from "../objects/styles"
+import { assembleCssForExport } from "../scripts/import-export-css"
+
 function Settings() {
 
   return (
     <>
       <button>import</button>
-      <button>export</button>
+      <button onClick={() => assembleCssForExport(masterStyles, null)}>export</button>
     </>
   )
 }
