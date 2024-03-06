0b1b09fe77061157407fea3e93de8cdae2da4144
Levi B <levib2000@gmail.com>
2024-03-06 02:31:31 -0500

    Add download to export buttton

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,11 +1,11 @@
 import { styles as masterStyles } from "../objects/styles"
-import { assembleCssForExport } from "../scripts/import-export-css"
+import { assembleCssForExport, handleDownloadClick } from "../scripts/import-export-css"
 
 function Settings() {
 
   return (
     <>
-      <button>import</button>
+      <button onClick={() => handleDownloadClick(null, null, masterStyles)}>import</button>
       <button onClick={() => assembleCssForExport(masterStyles, null)}>export</button>
     </>
   )
