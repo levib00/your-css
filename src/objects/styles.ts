9faaa0dd2d95fbe0a9aedd5c55153599ecd8dc68
Levi B <levib2000@gmail.com>
2024-03-07 22:41:18 -0500

    Ensure special styles keep unique properties

--- src/objects/styles.ts
+++ src/objects/styles.ts
@@ -12,6 +12,7 @@ export let styles: IStyle = {
     isActive: true,
     css: '',
     undeleteable: true,
+    displayName: 'toggle all'
   },
   _global: {
     isActive: false, 
@@ -23,10 +24,25 @@ export let styles: IStyle = {
     isActive: false,
     css: '',
     undeleteable: true,
-    displayName: 'extension style'
+    displayName: 'extension styles'
   },
 };
 
-export const setStyles = (newStyles: {[key: string] : {isActive: boolean, css: string}}) => {
+export const setStyles = (newStyles: {[key: string] : {isActive?: boolean, css?: string, undeleteable?: boolean, displayName?: string}}) => {
+  if (!newStyles._toggleAll) {
+    newStyles._toggleAll = styles._toggleAll
+  }
+  newStyles._toggleAll.undeleteable = true
+  newStyles._toggleAll.displayName = 'toggle all'
+  if (!newStyles._global) {
+    newStyles._global = styles._global
+  }
+  newStyles._global.undeleteable = true
+  newStyles._global.displayName = 'global styles'
+  if (!newStyles._extension) {
+    newStyles._extension = styles._extension
+  }
+  newStyles._extension.undeleteable = true
+  newStyles._extension.displayName = 'extension styles'
   styles = newStyles
 }
