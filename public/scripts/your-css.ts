c4d16c4465bb7f0213e0de37740106e23e620c3c
Levi B <levib2000@gmail.com>
2024-03-13 01:14:48 -0400

    Refactor so imports are not needed firefox content scripts cannot use imports. this moves all the imports to this file so they are unneeded.

--- public/scripts/your-css.ts
+++ public/scripts/your-css.ts
@@ -1,19 +1,70 @@
-import { styles, setStyles } from '../../src/objects/styles'
-import { getFromStorage } from '../../src/scripts/storage-handlers'
+interface IStyle {
+  [key: string] : {
+    isActive?: boolean,
+    css?: string
+    undeleteable?: boolean
+    displayName?: string
+  }
+};
+
+let styles: IStyle = {
+  ___toggleAll: {
+    isActive: true,
+    css: '',
+    undeleteable: true,
+    displayName: 'toggle all'
+  },
+  __global: {
+    isActive: false, 
+    css: '',
+    undeleteable: true,
+    displayName: 'global styles'
+  },
+  _extension: {
+    isActive: false,
+    css: '',
+    undeleteable: true,
+    displayName: 'extension styles'
+  },
+};
+
+const setStyles = (newStyles: {[key: string] : {isActive?: boolean, css?: string, undeleteable?: boolean, displayName?: string}}) => {
+  if (!newStyles.___toggleAll) {
+    newStyles.___toggleAll = styles.___toggleAll
+  }
+  newStyles.___toggleAll.undeleteable = true
+  newStyles.___toggleAll.displayName = 'toggle all'
+  if (!newStyles.__global) {
+    newStyles.__global = styles.__global
+  }
+  newStyles.__global.undeleteable = true
+  newStyles.__global.displayName = 'global styles'
+  if (!newStyles._extension) {
+    newStyles._extension = styles._extension
+  }
+  newStyles._extension.undeleteable = true
+  newStyles._extension.displayName = 'extension styles'
+  styles = newStyles
+  return styles
+}
+
+const getFromStorage = async(domain: string) => {
+  // @ts-ignore
+  const obj = await browser.storage.local.get(domain)
+  return obj[domain]
+}
 
 const domainStyle: HTMLStyleElement = document.createElement('style');
 
 const url = new URL(window.location.href);
 const domain: string | undefined = url.hostname.split('.')[0];
 
-setStyles(getFromStorage());
-
 interface Domain {
   isActive?: boolean
   css?: string
 };
 
-export const getStyleValue = (domain : Domain) => {
+const getStyleValue = (domain : Domain) => {
   let css = ''
   if ((!domain?.isActive && !styles.__global?.isActive) || !styles.___toggleAll?.isActive) {
     return ''
@@ -27,5 +78,10 @@ export const getStyleValue = (domain : Domain) => {
   return css
 }
 
-domainStyle.appendChild(document.createTextNode(getStyleValue(styles[domain]))); 
+(async() => {
+  await getFromStorage(domain).then((style) => {
+    domainStyle.appendChild(document.createTextNode(style.css))
+  })
+})()
+
 document.head.appendChild(domainStyle);
