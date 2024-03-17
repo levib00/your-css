429532cf722b09cc8419e0312ab51bb5c24340fe
Levi B <levib2000@gmail.com>
2024-03-17 02:39:18 -0400

    Remove React import

--- src/tests/home.test.tsx
+++ src/tests/home.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import Home from '../components/home';
 import { MemoryRouter } from 'react-router-dom';
