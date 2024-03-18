3a691a38517f6e82d551c0cb3fb7a01005321f74
Levi B <levib2000@gmail.com>
2024-03-18 04:21:02 -0400

    Fix clear listing for special styles

--- src/components/listing.tsx
+++ src/components/listing.tsx
@@ -15,11 +15,19 @@ function Listing(props: ListingProps) {
   const [editMode, setEditMode] = useState(false)
   const [isActive, setIsActive] = useState(styles.isActive)
 
-  const deleteListing = () => { // TODO: fix clear
+  const deleteListing = () => {
     const allStylesCopy = {...allStyles}
     delete allStylesCopy[style];
     // @ts-ignore
     browser.storage.local.remove(style)
+    setAllStyles(allStylesCopy)
+  }
+
+  const clearListing = () => {
+    const allStylesCopy = {...allStyles}
+    allStylesCopy[style].css = ''
+    // @ts-ignore
+    saveToStorage({[style]: allStylesCopy[style]})
     setAllStyles(allStylesCopy)
   }
 
@@ -55,7 +63,7 @@ function Listing(props: ListingProps) {
             { styles.css }
           </div>
           {style !== '___toggleAll' && <button onClick={openEditPage}>edit</button>}
-          {style !== '___toggleAll' && (styles.undeleteable ? <button onClick={deleteListing}>clear</button> : <button onClick={deleteListing}>delete</button>)}
+          {style !== '___toggleAll' && (styles.undeleteable ? <button onClick={clearListing}>clear</button> : <button onClick={deleteListing}>delete</button>)}
         </>
       }
     </>
