91b1aa3706ba38ed520d7f8f01ad2805a28e4d98
Levi B <levib2000@gmail.com>
2024-03-18 03:17:59 -0400

    Delete and checkbox update storage properly

--- src/components/listing.tsx
+++ src/components/listing.tsx
@@ -1,24 +1,26 @@
 import Form from "./form";
-import { IStyle, styles as masterStyles } from "../objects/styles";
-import { useLayoutEffect, useRef, useState } from "react";
+import { IStyle } from "../objects/styles";
+import { useEffect, useRef, useState } from "react";
 import { saveToStorage } from "../scripts/storage-handlers";
 
 interface ListingProps {
   style : string
   styles : {isActive?: boolean, css?: string, undeleteable?: boolean, displayName?: string} // TODO: fix these names.
   setAllStyles: React.Dispatch<React.SetStateAction<IStyle>>
+  allStyles: IStyle
 };
 
 function Listing(props: ListingProps) {
-  const {style, setAllStyles, styles} = props
+  const {style, setAllStyles, styles, allStyles} = props
   const [editMode, setEditMode] = useState(false)
   const [isActive, setIsActive] = useState(styles.isActive)
 
-  const deleteListing = () => {
-    delete masterStyles[style];
-    saveToStorage(masterStyles)
-    setAllStyles({...masterStyles})
-    // TODO: use DI instead of props.
+  const deleteListing = () => { // TODO: fix clear
+    const allStylesCopy = {...allStyles}
+    delete allStylesCopy[style];
+    // @ts-ignore
+    browser.storage.local.remove(style)
+    setAllStyles(allStylesCopy)
   }
 
   const openEditPage = () => {
@@ -27,15 +29,15 @@ function Listing(props: ListingProps) {
   }
 
   const firstUpdate = useRef(true);
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (firstUpdate.current) {
       firstUpdate.current = false;
       return;
     }
-
-    masterStyles[style].isActive = isActive
-    setAllStyles({...masterStyles})
-    saveToStorage(masterStyles)
+    const allStylesCopy = {...allStyles}
+    allStylesCopy[style].isActive = isActive
+    saveToStorage({[style]: allStylesCopy[style]})
+    setAllStyles({...allStylesCopy})
   }, [isActive]);
 
   return (
