d588b655a18c2179bd224f184cd89186c4486b58
Levi B <levib2000@gmail.com>
2024-03-19 03:33:42 -0400

    Add mock for global.browser.storage.local.remove

--- src/tests/setup/filemock.js
+++ src/tests/setup/filemock.js
@@ -1,8 +1,10 @@
 const mockStorageGet = jest.fn().mockResolvedValue({ key: 'mockedValue' });
+const mockStorageRemove = jest.fn()
 global.browser = {
   storage: {
     local: {
-      get: mockStorageGet
+      get: mockStorageGet,
+      remove: mockStorageRemove
     }
   }
 };
