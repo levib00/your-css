fc05e37f9c58869d580138aeda93740ea966dbb7
Levi B <levib2000@gmail.com>
2024-03-21 23:24:39 -0400

    Add mock for tabs.query

--- src/tests/setup/filemock.js
+++ src/tests/setup/filemock.js
@@ -1,6 +1,9 @@
 const mockStorageGet = jest.fn().mockResolvedValue({ key: 'mockedValue' });
 const mockStorageRemove = jest.fn()
 global.browser = {
+  tabs: {
+    query: jest.fn()
+  },
   storage: {
     local: {
       get: mockStorageGet,
