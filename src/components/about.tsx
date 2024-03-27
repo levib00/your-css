ddf38db966ce3f01636632520f4ec523c4926c0c
Levi B <levib2000@gmail.com>
2024-03-27 00:50:52 -0400

    Create about page

---
+++ src/components/about.tsx
@@ -0,0 +1,13 @@
+function About() {
+
+  return (
+    <div>
+      <a href="https://github.com/levib00/your-css">Source Code</a>
+      <a href="https://github.com/levib00/your-css/issues">Bugs & Reports</a>
+      <a>Releases</a>
+      <a href="https://www.buymeacoffee.com/levib00">Buy Me A Coffee</a>
+    </div>
+  )
+}
+
+export default About
