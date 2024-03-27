c746cbf71d06e47e5ff76101494e0cf28e6c54fd
Levi B <levib2000@gmail.com>
2024-03-27 00:51:38 -0400

    Add routes to about and settings

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import NavBar from './components/nav.js'
 import Form from "./components/form.js";
 import Home from "./components/home.js";
+import About from "./components/about.js";
+import Settings from "./components/settings.js";
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path="/form"  element={<Form /> } />
+          <Route path="/about"  element={<About /> } />
+          <Route path="/settings"  element={<Settings /> } />
         </Routes>
       </MemoryRouter>
     </div>
