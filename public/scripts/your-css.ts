60473bd15b8e358be745d811c549d732e256fcbf
Levi B <levib2000@gmail.com>
2024-03-29 04:10:47 -0400

    Linting

--- public/scripts/your-css.ts
+++ public/scripts/your-css.ts
@@ -1,58 +1,69 @@
-interface Domain {
-  isActive?: boolean,
-  css?: string
-  undeleteable?: boolean
-  displayName?: string
-};
+// interface Domain {
+//   isActive?: boolean,
+//   css?: string
+//   undeleteable?: boolean
+//   displayName?: string
+// }
 
-interface IStyle {
-  [key: string] : Domain
-};
+// interface IStyle {
+//   [key: string] : Domain
+// }
 
 (() => {
-  const getFromStorage = async(domain: string | undefined) => {
+  const getFromStorage = async (domain: string | undefined) => {
     if (!domain) {
-      return
+      return {};
     }
     // @ts-ignore
-    const domainStorage = await browser.storage.local.get(domain)
-    const domainObj = domainStorage[domain]
+    const domainStorage = await browser.storage.local.get(domain);
+    const domainObj = domainStorage[domain];
     // @ts-ignore
-    const ___toggleAllStorage = await browser.storage.local.get('___toggleAll')
-    const ___toggleAll = ___toggleAllStorage.___toggleAll
+    const toggleAllStorage = await browser.storage.local.get('___toggleAll');
+    const toggleAll = toggleAllStorage.___toggleAll;
     // @ts-ignore
-    const __globalStorage = await browser.storage.local.get('__global')
-    const __global = __globalStorage.__global
-    return {domain: domainObj, ___toggleAll, __global}
-  }
+    const globalStorage = await browser.storage.local.get('__global');
+    const global = globalStorage.__global;
+    return { domain: domainObj, ___toggleAll: toggleAll, __global: global };
+  };
 
   const domainStyle: HTMLStyleElement = document.createElement('style');
 
   const url = new URL(window.location.href);
   const domain: string | undefined = url.hostname;
 
-  const getStyleValue = (styles: IStyle | undefined) => {
+  const getStyleValue = (styles: {
+    [key: string]: {
+      isActive?: boolean,
+      css?: string,
+      undeleteable?: boolean,
+      displayName?: string, domain: string
+    } | undefined
+  } | undefined) => {
     if (!styles) {
-      return ''
+      return '';
     }
-    const {domain, ___toggleAll, __global} = styles
-    let css = ''
-    if ((!domain?.isActive && !__global?.isActive) || !___toggleAll?.isActive) {
-      return ''
-    } 
-    if (__global?.isActive && __global?.css) {
-      css = css.concat(__global.css)
+    const { domain: stylesForDomain, ___toggleAll: toggleAll, __global: global } = styles;
+    let css = '';
+    if ((!stylesForDomain?.isActive && !global?.isActive) || !toggleAll?.isActive) {
+      return '';
     }
-    if (domain?.isActive && domain.css){
-      css = css.concat(domain.css)
+    if (global?.isActive && global?.css) {
+      css = css.concat(global.css);
     }
-    return css
-  }
+    if (stylesForDomain?.isActive && stylesForDomain.css) {
+      css = css.concat(stylesForDomain.css);
+    }
+    return css;
+  };
 
-  (async() => {
-    const css = getStyleValue(await getFromStorage(domain))
-    domainStyle.appendChild(document.createTextNode(css))
-  })()
+  (async () => {
+    const stylesFromStorage = await getFromStorage(domain);
+    let css = '';
+    if (stylesFromStorage) {
+      css = getStyleValue(stylesFromStorage);
+    }
+    domainStyle.appendChild(document.createTextNode(css));
+  })();
 
   document.head.appendChild(domainStyle);
-})()
+})();
