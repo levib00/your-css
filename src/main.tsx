91cc336afdd779f876ac5279fecb4938255c367c
Levi B <levib2000@gmail.com>
2024-03-31 23:25:14 -0400

    Linting

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { styles } from './objects/styles'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { styles } from './objects/styles';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
 
 const domainStyle: HTMLStyleElement = document.createElement('style');
 domainStyle.appendChild(document.createTextNode(styles._extension?.isActive && styles._extension?.css ? styles._extension?.css : ''));
