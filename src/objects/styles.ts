39828cac3742fabd070c60ca07812211c3af5ef1
Levi B <levib2000@gmail.com>
2024-04-01 00:23:05 -0400

    Remove unused function setStyles

--- src/objects/styles.ts
+++ src/objects/styles.ts
@@ -27,33 +27,3 @@ export let styles: IStyle = {
     displayName: 'extension styles',
   },
 };
-
-export const setStyles = (
-  newStyles: {
-    [key: string] : {
-      isActive?: boolean,
-      css?: string,
-      undeleteable?:
-      boolean,
-      displayName?: string
-    }
-  },
-) => {
-  const newStyleCopy = newStyles
-  if (!newStyleCopy.___toggleAll) {
-    newStyleCopy.___toggleAll = styles.___toggleAll;
-  }
-  newStyleCopy.___toggleAll.undeleteable = true;
-  newStyleCopy.___toggleAll.displayName = 'toggle all';
-  if (!newStyleCopy.__global) {
-    newStyleCopy.__global = styles.__global;
-  }
-  newStyleCopy.__global.undeleteable = true;
-  newStyleCopy.__global.displayName = 'global styles';
-  if (!newStyleCopy._extension) {
-    newStyleCopy._extension = styles._extension;
-  }
-  newStyleCopy._extension.undeleteable = true;
-  newStyleCopy._extension.displayName = 'extension styles';
-  styles = newStyleCopy;
-};
