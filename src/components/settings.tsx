5b815315a7d553232ff211eb908cbbe017fd64cf
Levi B <levib2000@gmail.com>
2024-04-02 02:24:13 -0400

    Refactor to use storage instead of masterStyles

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,11 +1,21 @@
-import { styles as masterStyles } from '../objects/styles';
+import { useEffect, useState } from 'react';
 import { assembleCssForExport, handleDownloadClick } from '../scripts/import-export-css';
-// TODO: fix handleDownloadClick
+import { getFromStorage, populateSpecialStyles } from '../scripts/storage-handlers';
+import { IStyle } from '../objects/styles';
+
 function Settings() {
+  const [allStyles, setAllStyles] = useState<IStyle | undefined>();
+
+  useEffect(() => {
+    (async () => {
+      setAllStyles(populateSpecialStyles(await getFromStorage(null)));
+    })();
+  });
+
   return (
     <>
-      <button onClick={() => handleDownloadClick(null, null, masterStyles)}>import</button>
-      <button onClick={() => assembleCssForExport(masterStyles, null)}>export</button>
+      <button onClick={() => handleDownloadClick(null, null, allStyles)}>import</button>
+      <button onClick={() => assembleCssForExport(allStyles, null)}>export</button>
     </>
   );
 }
