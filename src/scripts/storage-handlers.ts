947ff925bdc85731702a699fb4bf8f36ea2bfa3e
Levi B <levib2000@gmail.com>
2024-04-01 23:58:48 -0400

    Change styles import name

--- src/scripts/storage-handlers.ts
+++ src/scripts/storage-handlers.ts
@@ -1,4 +1,4 @@
-import { styles } from '../objects/styles';
+import { defaultStyles } from '../objects/styles';
 
 export const saveToStorage = (
   newObject : { [key: string] : { isActive?: boolean, css?: string } },
@@ -22,17 +22,17 @@ export const populateSpecialStyles = (
 ) => {
   const newStyleCopy = newStyle;
   if (!newStyleCopy.___toggleAll) {
-    newStyleCopy.___toggleAll = styles.___toggleAll;
+    newStyleCopy.___toggleAll = defaultStyles.___toggleAll;
   }
   newStyleCopy.___toggleAll.undeleteable = true;
   newStyleCopy.___toggleAll.displayName = 'toggle all';
   if (!newStyleCopy.__global) {
-    newStyleCopy.__global = styles.__global;
+    newStyleCopy.__global = defaultStyles.__global;
   }
   newStyleCopy.__global.undeleteable = true;
   newStyleCopy.__global.displayName = 'global styles';
   if (!newStyleCopy._extension) {
-    newStyleCopy._extension = styles._extension;
+    newStyleCopy._extension = defaultStyles._extension;
   }
   newStyleCopy._extension.undeleteable = true;
   newStyleCopy._extension.displayName = 'extension styles';
