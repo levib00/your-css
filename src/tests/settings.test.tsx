54512064aae216c9d8fb3e2d066a28b513ce2c1c
Levi B <levib2000@gmail.com>
2024-04-04 04:23:53 -0400

    Adjust tests so component uses storage Adjust tests so component uses storage instead of an object. Since getting from storage is asynchronous added a test for loading. waitFor data to be gotten where needed.

--- src/tests/settings.test.tsx
+++ src/tests/settings.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import { act } from 'react-dom/test-utils';
 import userEvent from '@testing-library/user-event';
 import * as importExport from '../scripts/import-export-css';
-import { setStyles } from '../objects/styles';
 import Settings from '../components/settings';
 
 describe('Settings page component', () => {
@@ -15,13 +14,46 @@ describe('Settings page component', () => {
       </MemoryRouter>,
     );
 
-    const importButton = screen.getByText('import');
+    waitFor(async () => {
+      const importButton = screen.getByText('import');
+      expect(importButton).toBeInTheDocument();
+      const exportButton = screen.getByText('export');
+      expect(exportButton).toBeInTheDocument();
+    });
+  });
+
+  test('Renders loading', () => {
+    render(
+      <MemoryRouter>
+        <Settings />
+      </MemoryRouter>,
+    );
+
+    const importButton = screen.getByText('Loading...');
     expect(importButton).toBeInTheDocument();
-    const exportButton = screen.getByText('export');
-    expect(exportButton).toBeInTheDocument();
   });
 
   test('export fires', async () => {
+    const masterStylesMock = {
+      styleName: { isActive: true, css: 'style text' },
+      styleName2: { isActive: true, css: 'style text 2' },
+    };
+
+    const mockStorageGet = jest.fn().mockResolvedValue(masterStylesMock);
+    const mockStorageRemove = jest.fn();
+    // @ts-ignore
+    global.browser = {
+      tabs: {
+        query: jest.fn(),
+      },
+      storage: {
+        local: {
+          get: mockStorageGet,
+          remove: mockStorageRemove,
+        },
+      },
+    };
+
     const mock = jest.spyOn(importExport, 'assembleCssForExport').mockImplementation(jest.fn());
     render(
       <MemoryRouter>
@@ -29,22 +61,16 @@ describe('Settings page component', () => {
       </MemoryRouter>,
     );
 
-    const masterStylesMock = {
-      styleName: { isActive: true, css: 'style text' },
-      styleName2: { isActive: true, css: 'style text 2' },
-    };
-
-    setStyles(masterStylesMock);
-
     jest.spyOn(importExport, 'assembleCssForExport').mockImplementationOnce(() => 'Updated');
 
-    const exportButton = screen.getByText('export');
-    expect(exportButton).toBeInTheDocument();
+    waitFor(async () => {
+      const exportButton = screen.getByText('export');
+      expect(exportButton).toBeInTheDocument();
 
-    await act(async () => {
-      await userEvent.click(exportButton);
+      await act(async () => {
+        await userEvent.click(exportButton);
+      });
+      expect(mock).toHaveBeenCalledWith(masterStylesMock, null);
     });
-
-    expect(mock).toHaveBeenCalledWith(masterStylesMock, null);
   });
 });
