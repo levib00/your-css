7ae38d833f8028adb4deeb6ab30b8243ba06fd1b
Levi B <levib2000@gmail.com>
2024-04-06 02:33:40 -0400

    Adjust test for refactored component

--- src/tests/home.test.tsx
+++ src/tests/home.test.tsx
@@ -2,41 +2,38 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import Home from '../components/home';
 
 describe('Home renders', () => {
   test('Home renders with correct text', async () => {
-    await act(async () => {
-      render(
-        <MemoryRouter>
-          <Home />
-        </MemoryRouter>,
-      );
-    });
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
 
-    const globalStyles = screen.getByText('global styles');
-    expect(globalStyles).toBeInTheDocument();
-    const toggleAll = screen.getByText('toggle all');
-    expect(toggleAll).toBeInTheDocument();
-    const extensionStyles = screen.getByText('extension styles');
-    expect(extensionStyles).toBeInTheDocument();
+    waitFor(() => {
+      const globalStyles = screen.getByText('global styles');
+      expect(globalStyles).toBeInTheDocument();
+      const toggleAll = screen.getByText('toggle all');
+      expect(toggleAll).toBeInTheDocument();
+      const extensionStyles = screen.getByText('extension styles');
+      expect(extensionStyles).toBeInTheDocument();
+    });
   });
 
-  test('Search bar works', async () => {
-    await act(async () => {
-      render(
-        <MemoryRouter>
-          <Home />
-        </MemoryRouter>,
-      );
-    });
+  test('Search bar works', () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
     const searchBar = screen.getByPlaceholderText('website');
     expect(searchBar).toBeInTheDocument();
 
-    await userEvent.type(searchBar, 'toggle');
+    userEvent.type(searchBar, 'toggle');
 
-    await waitFor(() => {
+    waitFor(() => {
       const exampleName1 = screen.getByText('toggle all');
       expect(exampleName1).toBeInTheDocument();
       const exampleName2 = screen.queryByText('global styles');
