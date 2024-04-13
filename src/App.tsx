c5b931be552f8d1a2621119b53ab5a6dc3378ff1
Levi B <levib2000@gmail.com>
2024-04-12 23:19:51 -0400

    Add state for dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, MemoryRouter } from 'react-router-dom';
 import './App.css';
+import { useEffect, useState } from 'react';
 import NavBar from './components/nav.js';
 import Form from './components/form.js';
 import Home from './components/home.js';
@@ -7,15 +8,24 @@ import About from './components/about.js';
 import Settings from './components/settings.js';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+
+  useEffect(() => {
+    (async () => {
+      const darkModeStorage = await browser.storage.local.get('darkMode');
+      setIsDarkMode(await darkModeStorage.darkMode || false);
+    })();
+  }, []);
+
   return (
-    <div>
+    <div className={isDarkMode ? 'dark-mode' : ''}>
       <MemoryRouter>
         <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path="/form" element={<Form /> } />
           <Route path="/about" element={<About /> } />
-          <Route path="/settings" element={<Settings /> } />
+          <Route path="/settings" element={<Settings setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode} /> } />
         </Routes>
       </MemoryRouter>
     </div>
