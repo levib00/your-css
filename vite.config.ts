444d48bf66249e29c6f4bd2a81312204900c7082
Levi B <levib2000@gmail.com>
2024-05-26 02:20:36 -0400

    Adjust vite.config for having 2 html files

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        options_ui: 'import-listing.html'
+      }
+    },
+  },
   plugins: [react()],
 })
