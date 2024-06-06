839d816e938650a8381fb19beac34fd20fdc492c
Levi B <levib2000@gmail.com>
2024-06-06 02:20:36 -0400

    Fix downloadClick to return undefined if no file

--- src/scripts/import-export-css.ts
+++ src/scripts/import-export-css.ts
@@ -3,7 +3,7 @@ import { IStyle } from '../objects/styles';
 export const assembleCssForExport = (
   allStyles: IStyle | null | undefined,
   css: string | null,
-): string => {
+) => {
   if (!allStyles && !css) {
     return '';
   }
@@ -14,29 +14,11 @@ export const assembleCssForExport = (
   return URL.createObjectURL(file);
 };
 
-export const parseJsonFile = async (
-  file: File | undefined,
-  allStyles: IStyle
-): Promise<IStyle> => {
-  if (!file) {
-    return allStyles;
-  }
-
-  try {
-    const json = await file.text();
-    const parsedJSON = JSON.parse(json);
-    return { ...parsedJSON, ...allStyles };
-  } catch (error) {
-    console.error('Failed to parse JSON file', error);
-    return allStyles;
-  }
-};
-
 export const handleDownloadClick = (
   css: string | null,
   domain: string | null,
   allStyles: IStyle | null | undefined,
-): void => {
+) => {
   const url = assembleCssForExport(allStyles, css);
   if (!url) {
     return;
